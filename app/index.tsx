1f86b8916305537c21053a11c677f816c55a68ca
Téo <teomullerheddar@gmail.com>
2025-06-13 10:25:29 +0200

    Add Articles Screen
    page pour afficher la listes des articles
    
    A prévoir: Mettre la page dans un dossier screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import Post from '@/components/Post';
+import { Link } from 'expo-router';
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+
+    const { data, error, isLoading } = useSWR('https://api.crudme.mindlens.fr/posts', fetcher);
+
+    if (isLoading) return <Text>Chargement...</Text>;
+    if (error) return <Text>Erreur : {error.message}</Text>;
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.list}>
+                {data.posts.map((post: any) => (
+                    <Link href={`/post/${post.id}`} key={post.id}>
+                        <Post post={post} />
+                    </Link>
+                ))}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+    container: {
+        backgroundColor: '#f0f0f0',
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+    },
+    list: {
+        width: '100%',
+    }
 });
---
+++ src/components/Post.tsx
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    content: {
+        fontSize: 14,
+    },
+    container: {
+        width: '100%',
+        padding: 8,
+    },
+    post: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+    },
+});
+
+export default function Post({ post }: { post: any }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.post}>
+                <Text style={styles.title}>{post.title}</Text>
+                <Text style={styles.content}>{post.content}</Text>
+            </View>
+        </View>
+    );
+}
