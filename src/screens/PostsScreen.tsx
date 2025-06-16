03fd38d22e36283a5c10f02579c235e53c452a11
Téo <teomullerheddar@gmail.com>
2025-06-16 23:35:26 +0200

    JWT Fonctionnel

--- src/screens/PostsScreen.tsx
+++ src/screens/PostsScreen.tsx
@@ -1,23 +1,45 @@
-import { SafeAreaView, StyleSheet } from "react-native";
-import { Text } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useAuth } from "@/providers/AuthProvider";
+import { useState } from "react";
+import { Link } from "expo-router";
+import Post from "@/components/Post";
 
 export default function PostsScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>PostsScreen</Text>
-    </SafeAreaView>
-  );
+
+    const [posts, setPosts] = useState([]);
+
+    const { token } = useAuth();
+
+    axios.get(process.env.EXPO_PUBLIC_API_URL + `/posts?page=1&limit=10`, {
+            headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }).then((res) => {
+        setPosts(res.data.posts);
+    });
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.list}>
+            {posts.map((post: any) => (
+                <Link href={`/post/${post.id}`} key={post.id}>
+                    <Post post={post} />
+                </Link>
+            ))}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+    container: {
+        backgroundColor: '#f0f0f0',
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+    },
+    list: {
+        width: '100%',
+    },
 });
