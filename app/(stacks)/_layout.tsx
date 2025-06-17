170aa0c388e1465acc74c6ed3212744b3d840cf8
Téo <teomullerheddar@gmail.com>
2025-06-17 21:23:41 +0200

    Adding SinglePostScreen

--- app/(stacks)/_layout.tsx
+++ app/(stacks)/_layout.tsx
@@ -10,7 +10,6 @@ export default function StackLayout() {
                     headerBackTitle: 'Retour',
                     title: 'Article',
                 }}
-
             />
         </Stack>
     );
---
+++ src/screens/SoloPostScreen.tsx
@@ -0,0 +1,80 @@
+import { Stack } from "expo-router";
+import { View, Text, ScrollView, Button, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth } from "@/providers/AuthProvider";
+
+export default function SoloPostScreen(props: { id: string }) {
+    const { id } = props;
+    const { token } = useAuth();
+    const [post, setPost] = useState<any>(null);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleContent = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    useEffect(() => {
+        if (token) {
+            axios.get(`${process.env.EXPO_PUBLIC_API_URL}/posts/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }).then((res) => {
+                setPost(res.data.post);
+            });
+        }
+    }, [token]);
+
+    return (
+        <ScrollView style={styles.container}>
+            <Stack.Screen options={{ headerTitle: "Article", headerBackTitle: "Retour" }} />
+            <View style={styles.content}>
+                <Text style={styles.title}>{post?.title}</Text>
+                <View style={styles.calendar}>
+                    <Text style={styles.calendarText}>{post?.created_at}</Text>
+                </View>
+                <View style={styles.contentContainer}>
+                    <Text style={styles.contentText}>{isExpanded ? post?.content : post?.content.slice(0, 100) + "..."}</Text>
+                    <Button title={isExpanded ? "Voir Moins" : "Voir Plus"} onPress={toggleContent} />
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f0f0f0",
+        padding: 12,
+    },
+    content: {
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#000",
+        marginBottom: 20,
+    },
+    calendar: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+    calendarText: {
+        fontSize: 16,
+        color: "#000",
+    },
+    contentContainer: {
+        marginTop: 20,
+    },
+    contentText: {
+        fontSize: 16,
+        color: "#000",
+        textAlign: "justify",
+    }
+})
