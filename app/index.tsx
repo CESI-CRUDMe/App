6769593568decc8eb558cf415ac650dfa4460858
Téo <teomullerheddar@gmail.com>
2025-08-26 19:08:12 +0200

    fix(login): fix page login / route

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,6 @@
-import { useUser } from '@/providers/AuthProvider';
-import { Redirect } from 'expo-router';
+import LoginScreen from '@/screens/LoginScreen';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
 
-export default function Index() {
-  const { isLogged, loading } = useUser();
-
-  if (loading) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ActivityIndicator />
-      </View>
-    );
-  }
-
-  return <Redirect href={isLogged ? '/dashboard' : '/login'} />;
+export default function LoginTab() {
+    return <LoginScreen />;
 }
