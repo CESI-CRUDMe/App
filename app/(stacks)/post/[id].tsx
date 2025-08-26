42dd1d1d2e12a3dada40a8d9575671532fa5f26c
Téo <teomullerheddar@gmail.com>
2025-08-26 08:52:01 +0200

    remove(gen): reset old project

--- app/(stacks)/post/[id].tsx
+++
@@ -1,9 +0,0 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
-import PostScreen from '@/screens/articles/PostScreen';
-
-export default function Post() {
-    const { id } = useLocalSearchParams();
-    return (
-        <PostScreen id={id as string} />
-    );
-}
--- app/index.tsx
+++
@@ -1,5 +0,0 @@
-import ArticlesScreen from "@/screens/articles/ArticlesScreen";
-
-export default function Index() {
-    return <ArticlesScreen />;
-}
